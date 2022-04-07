2ed15952f4b585cedc720cc10d5307c15d1653aa
Manny Singh <likebeats@gmail.com>
2022-04-07 00:42:46 -0400

    locations api

--- pages/api/maps/location.ts
+++ pages/api/maps/location.ts
@@ -2,7 +2,7 @@ import { IncomingMessage } from 'http';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
 import { ParsedUrlQuery } from 'querystring';
-import { JSONResponse } from '../../../interfaces/models';
+import { JSONResponse, Location } from '../../../interfaces/models';
 
 export default handler;
 
@@ -29,7 +29,7 @@ export const getLocation = async ({
 }: {
     req: IncomingMessage;
     query: ParsedUrlQuery;
-}): Promise<JSONResponse<{ location: any }>> => {
+}): Promise<JSONResponse<{ locations: Location[] }>> => {
     const session = await getSession({ req });
 
     if (!session || !session.user) {
@@ -39,11 +39,42 @@ export const getLocation = async ({
         };
     }
 
-    // https://api.mapbox.com/geocoding/v5/mapbox.places/<text>.json?access_token=<token>&types=address&country=us%2Cca
-    console.log('search', query.q);
+    const params = new URLSearchParams({
+        access_token: process.env.MAPBOX_TOKEN,
+        types: 'address',
+        country: 'us,ca',
+        proximity: 'ip',
+    });
+
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query.q}.json?${params.toString()}`;
+    const response = await fetch(url);
+    const json = await response.json();
+
+    console.log('json', json);
+
+    const findValueInContext = (name: string, context: any[], keyToReturn = 'text'): string | number => {
+        const feature = context.find((x) => x.id.includes(`${name}.`));
+        if (!feature) {
+            return null;
+        }
+        return feature[keyToReturn];
+    };
+
+    const locations: Location[] = json?.features?.map((feature: any) => {
+        const countryCode = findValueInContext('country', feature.context, 'short_code');
+        return {
+            street: feature.address + ' ' + feature.text,
+            city: findValueInContext('place', feature.context),
+            state: findValueInContext('region', feature.context),
+            zip: findValueInContext('postcode', feature.context),
+            country: countryCode ? (countryCode as string).toUpperCase() : '',
+            longitude: feature.center[0],
+            latitude: feature.center[1],
+        } as Location;
+    });
 
     return {
         code: 200,
-        data: { location: {} },
+        data: { locations: locations },
     };
 };
