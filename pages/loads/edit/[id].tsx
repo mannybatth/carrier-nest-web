04d2d89934eb9e960fe4bc9eb97b238fac69a0c7
Manny Singh <likebeats@gmail.com>
2022-04-09 02:33:34 -0400

    add load save confirmation

--- pages/loads/edit/[id].tsx
+++ pages/loads/edit/[id].tsx
@@ -7,6 +7,7 @@ import safeJsonStringify from 'safe-json-stringify';
 import LoadForm from '../../../components/forms/load/LoadForm';
 import BreadCrumb from '../../../components/layout/BreadCrumb';
 import Layout from '../../../components/layout/Layout';
+import SaveLoadConfirmation from '../../../components/loads/SaveLoadConfirmation';
 import { notify } from '../../../components/Notification';
 import { PageWithAuth } from '../../../interfaces/auth';
 import type { ExpandedLoad, SimpleLoadStop } from '../../../interfaces/models';
@@ -51,6 +52,9 @@ const EditLoad: PageWithAuth<Props> = ({ load: loadProp }: Props) => {
     const [loading, setLoading] = React.useState(false);
     const [load, setLoad] = React.useState<ExpandedLoad>(loadProp);
 
+    const [showConfirmation, setShowConfirmation] = React.useState(false);
+    const [dataToSave, setDataToSave] = React.useState<ExpandedLoad>(null);
+
     useEffect(() => {
         if (!load) {
             formHook.reset();
@@ -75,7 +79,14 @@ const EditLoad: PageWithAuth<Props> = ({ load: loadProp }: Props) => {
         console.log('prop load', loadProp);
         console.log('data to save', data);
 
-        setLoading(true);
+        const needsConfirmation =
+            !data.shipper.longitude ||
+            !data.shipper.latitude ||
+            !data.receiver.longitude ||
+            !data.receiver.latitude ||
+            !data.stops.every((stop: SimpleLoadStop) => stop.longitude && stop.latitude);
+
+        console.log('needs confirmation', needsConfirmation);
 
         const loadData: ExpandedLoad = {
             customerId: data.customer.id,
@@ -103,6 +114,17 @@ const EditLoad: PageWithAuth<Props> = ({ load: loadProp }: Props) => {
             });
         }
 
+        if (needsConfirmation) {
+            setDataToSave(loadData);
+            setShowConfirmation(true);
+        } else {
+            await saveLoadData(loadData);
+        }
+    };
+
+    const saveLoadData = async (loadData: ExpandedLoad) => {
+        setLoading(true);
+
         const newLoad = await updateLoad(load.id, loadData);
         console.log('updated load', newLoad);
 
@@ -116,42 +138,45 @@ const EditLoad: PageWithAuth<Props> = ({ load: loadProp }: Props) => {
 
     return (
         <Layout smHeaderComponent={<h1 className="text-xl font-semibold text-gray-900">Edit Load</h1>}>
-            <div className="max-w-4xl py-2 mx-auto">
-                <BreadCrumb
-                    className="sm:px-6 md:px-8"
-                    paths={[
-                        {
-                            label: 'Loads',
-                            href: '/loads',
-                        },
-                        {
-                            label: `# ${load.refNum}`,
-                            href: `/loads/${load.id}`,
-                        },
-                        {
-                            label: 'Edit Load',
-                        },
-                    ]}
-                ></BreadCrumb>
-                <div className="hidden px-5 my-4 md:block sm:px-6 md:px-8">
-                    <h1 className="text-2xl font-semibold text-gray-900">Edit Load</h1>
-                    <div className="w-full mt-2 mb-1 border-t border-gray-300" />
+            <>
+                <SaveLoadConfirmation show={showConfirmation} onClose={() => setShowConfirmation(false)} />
+                <div className="max-w-4xl py-2 mx-auto">
+                    <BreadCrumb
+                        className="sm:px-6 md:px-8"
+                        paths={[
+                            {
+                                label: 'Loads',
+                                href: '/loads',
+                            },
+                            {
+                                label: `# ${load.refNum}`,
+                                href: `/loads/${load.id}`,
+                            },
+                            {
+                                label: 'Edit Load',
+                            },
+                        ]}
+                    ></BreadCrumb>
+                    <div className="hidden px-5 my-4 md:block sm:px-6 md:px-8">
+                        <h1 className="text-2xl font-semibold text-gray-900">Edit Load</h1>
+                        <div className="w-full mt-2 mb-1 border-t border-gray-300" />
+                    </div>
+                    <div className="px-5 mb-64 sm:px-6 md:px-8">
+                        <form id="load-form" onSubmit={formHook.handleSubmit(submit)}>
+                            <LoadForm formHook={formHook}></LoadForm>
+                            <div className="flex px-4 py-4 mt-4 bg-white border-t-2 border-neutral-200">
+                                <div className="flex-1"></div>
+                                <button
+                                    type="submit"
+                                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                                >
+                                    Save Load
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div className="px-5 mb-64 sm:px-6 md:px-8">
-                    <form id="load-form" onSubmit={formHook.handleSubmit(submit)}>
-                        <LoadForm formHook={formHook}></LoadForm>
-                        <div className="flex px-4 py-4 mt-4 bg-white border-t-2 border-neutral-200">
-                            <div className="flex-1"></div>
-                            <button
-                                type="submit"
-                                className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                            >
-                                Save Load
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            </>
         </Layout>
     );
 };
