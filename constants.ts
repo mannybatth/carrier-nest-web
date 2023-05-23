4f57e67357e8be4a9d4f49dee08a6f4b113dc051
Manny Singh <likebeats@gmail.com>
2023-05-22 23:56:00 -0400

    update constant for production

--- constants.ts
+++ constants.ts
@@ -1,4 +1,5 @@
+// Use localhost for development and url from browser for production
 export const apiUrl =
     process.env.NODE_ENV === 'development'
         ? 'http://localhost:3000/api' // development api
-        : 'http://localhost:3000/api'; // production api
+        : window.location.origin + '/api'; // production api
