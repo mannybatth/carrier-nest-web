cde2f7dc0295330cff2f399b280e28cdbdbc0de7
Manny Singh <likebeats@gmail.com>
2023-05-23 10:33:53 -0400

    revert autoprefixer package

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,5 +29,5 @@ module.exports = {
             },
         },
     },
-    plugins: [require('@tailwindcss/line-clamp'), require('@tailwindcss/forms')],
+    plugins: [require('@tailwindcss/forms')],
 };
