72d847d596e27f4a38bd942f6ece1e9b1039965c
Manny Singh <likebeats@gmail.com>
2023-06-19 04:25:30 -0400

    add error safety

--- pages/api/ai/ratecon.ts
+++ pages/api/ai/ratecon.ts
@@ -11,26 +11,37 @@ export const config = {
 };
 
 export default async function POST(req: NextRequest) {
-    const list = await req.json();
+    try {
+        const list = await req.json();
 
-    let documents: Document[] = list.map((item) => {
-        return new Document({
-            pageContent: item.pageContent,
-            metadata: item.metadata,
+        if (!Array.isArray(list)) {
+            throw new Error('Invalid input. Expected an array.');
+        }
+
+        const documents: Document[] = list.map((item) => {
+            if (!isValidItem(item)) {
+                throw new Error('Invalid item in the list.');
+            }
+
+            return new Document({
+                pageContent: item.pageContent,
+                metadata: item.metadata,
+            });
         });
-    });
 
-    // Split text into characters
-    const splitter = new CharacterTextSplitter({
-        chunkSize: 1000,
-        chunkOverlap: 200,
-    });
-    documents = await splitter.splitDocuments(documents);
-    const vectordb = await MemoryVectorStore.fromDocuments(documents, new OpenAIEmbeddings());
-    const qaChain = RetrievalQAChain.fromLLM(new ChatOpenAI(), vectordb.asRetriever(7), {
-        returnSourceDocuments: false,
-    });
-    const prompt = `For the given text of a Carrier Rate and Load Confirmation pdf, provide a JSON representation of the document. All values should be present in the document, do not make any assumptions about the values.
+        const splitter = new CharacterTextSplitter({
+            chunkSize: 1000,
+            chunkOverlap: 200,
+        });
+
+        const splitDocuments = await splitter.splitDocuments(documents);
+        const vectordb = await MemoryVectorStore.fromDocuments(splitDocuments, new OpenAIEmbeddings());
+
+        const qaChain = RetrievalQAChain.fromLLM(new ChatOpenAI(), vectordb.asRetriever(7), {
+            returnSourceDocuments: false,
+        });
+
+        const prompt = `For the given text of a Carrier Rate and Load Confirmation pdf, provide a JSON representation of the document. All values should be present in the document, do not make any assumptions about the values.
 The JSON representation should include the following fields:
 - "logistics_company": The name of the logistics company
 - "load_number": The load/reference number for the shipment
@@ -45,28 +56,60 @@ The JSON representation should include the following fields:
 - "rate": The rate for the shipment. Only include the number, not the currency
 - "invoice_email": The email address where to send proof of delivery (POD/BOL) and invoice after delivery. If there is no email address, return "null"
 `;
-    const result = await qaChain.call({
-        query: prompt,
-    });
 
-    if (typeof result.text === 'string') {
-        const jsonStr = result.text?.replace(/`/g, '');
-        const json = JSON.parse(jsonStr);
+        const result = await qaChain.call({
+            query: prompt,
+        });
 
+        if (typeof result.text === 'string') {
+            const jsonStr = result.text?.replace(/`/g, '');
+
+            try {
+                const json = JSON.parse(jsonStr);
+                return NextResponse.json(
+                    {
+                        code: 200,
+                        data: json,
+                    },
+                    { status: 200 },
+                );
+            } catch (error) {
+                return NextResponse.json(
+                    {
+                        code: 200,
+                        error: jsonStr,
+                    },
+                    { status: 200 },
+                );
+            }
+        } else {
+            return NextResponse.json(
+                {
+                    code: 200,
+                    data: result.text,
+                },
+                { status: 200 },
+            );
+        }
+    } catch (error) {
         return NextResponse.json(
             {
-                code: 200,
-                data: json,
+                code: 400,
+                error: error.message,
             },
-            { status: 200 },
-        );
-    } else {
-        return NextResponse.json(
-            {
-                code: 200,
-                data: result.text,
-            },
-            { status: 200 },
+            { status: 400 },
         );
     }
+}
+
+function isValidItem(item: Document): boolean {
+    if (typeof item.pageContent !== 'string') {
+        return false;
+    }
+
+    if (typeof item.metadata !== 'object') {
+        return false;
+    }
+
+    return true;
 }
