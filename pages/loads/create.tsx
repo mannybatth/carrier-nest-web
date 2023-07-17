6419478cf75783a8d0e3862beeab7dcf7719428e
Manny Singh <likebeats@gmail.com>
2023-07-17 01:11:17 -0400

    remove address verification on load creation

--- pages/loads/create.tsx
+++ pages/loads/create.tsx
@@ -1,4 +1,4 @@
-import { LoadStop, LoadStopType, Prisma } from '@prisma/client';
+import { LoadStopType, Prisma } from '@prisma/client';
 import { useRouter } from 'next/router';
 import React from 'react';
 import { useForm } from 'react-hook-form';
@@ -9,13 +9,9 @@ import { notify } from '../../components/Notification';
 import { PageWithAuth } from '../../interfaces/auth';
 import { ExpandedLoad } from '../../interfaces/models';
 import { createLoad } from '../../lib/rest/load';
-import SaveLoadConfirmation from '../../components/loads/SaveLoadConfirmation';
 import { parsePdf, AILoad } from '../../lib/rest/ai';
 import { LoadingOverlay } from '../../components/LoadingOverlay';
 import { searchCustomersByName } from '../../lib/rest/customer';
-import { queryLocations } from '../../lib/rest/maps';
-import { LocationEntry, regionFromLocationEntry } from '../../interfaces/location';
-import AddressConfirmation from '../../components/loads/AddressConfirmation';
 import { FileUploader } from 'react-drag-drop-files';
 
 const CreateLoad: PageWithAuth = () => {
@@ -24,23 +20,8 @@ const CreateLoad: PageWithAuth = () => {
 
     const [loading, setLoading] = React.useState(false);
 
-    const [showConfirmation, setShowConfirmation] = React.useState(false);
-    const [dataToSave, setDataToSave] = React.useState<ExpandedLoad>(null);
-
-    const [aiLoad, setAILoad] = React.useState<AILoad>(null);
-    const [showAddressConfirmation, setShowAddressConfirmation] = React.useState(false);
-    const [shipperLocations, setShipperLocations] = React.useState<LocationEntry[]>([]);
-    const [receiverLocations, setReceiverLocations] = React.useState<LocationEntry[]>([]);
-
     const submit = async (data: ExpandedLoad) => {
         console.log(data);
-
-        const needsConfirmation =
-            !data.shipper.longitude ||
-            !data.shipper.latitude ||
-            !data.receiver.longitude ||
-            !data.receiver.latitude ||
-            !data.stops.every((stop: LoadStop) => stop.longitude && stop.latitude);
 
         data.shipper.type = LoadStopType.SHIPPER;
         data.receiver.type = LoadStopType.RECEIVER;
@@ -56,12 +37,7 @@ const CreateLoad: PageWithAuth = () => {
             stops: data.stops,
         };
 
-        if (needsConfirmation) {
-            setDataToSave(loadData);
-            setShowConfirmation(true);
-        } else {
-            await saveLoadData(loadData);
-        }
+        await saveLoadData(loadData);
     };
 
     const saveLoadData = async (loadData: ExpandedLoad) => {
@@ -91,7 +67,6 @@ const CreateLoad: PageWithAuth = () => {
             try {
                 setLoading(true);
                 const load = await parsePdf(byteArray, file);
-                setAILoad(load);
                 await applyAIOutputToForm(load);
                 setLoading(false);
             } catch (e) {
@@ -123,13 +98,7 @@ const CreateLoad: PageWithAuth = () => {
         formHook.setValue('receiver.date', load.delivery_date ? new Date(load.delivery_date) : null);
         formHook.setValue('receiver.time', load.delivery_time);
 
-        const [{ shipperLocations, receiverLocations }] = await Promise.all([
-            findAddressesFromOutput(load),
-            setCustomerFromOutput(load),
-        ]);
-        setShipperLocations(shipperLocations);
-        setReceiverLocations(receiverLocations);
-        setShowAddressConfirmation(true);
+        setCustomerFromOutput(load);
     };
 
     const setCustomerFromOutput = async (load: AILoad) => {
@@ -151,72 +120,8 @@ const CreateLoad: PageWithAuth = () => {
         }
     };
 
-    const findAddressesFromOutput = async (load: AILoad) => {
-        const promises: Promise<LocationEntry[]>[] = [];
-        if (load.shipper_address) {
-            const query = `${load.shipper_address.street} ${load.shipper_address.city} ${load.shipper_address.state} ${load.shipper_address.zip}`;
-            promises.push(queryLocations(query));
-        } else {
-            promises.push(Promise.resolve([]));
-        }
-
-        if (load.consignee_address) {
-            const query = `${load.consignee_address.street} ${load.consignee_address.city} ${load.consignee_address.state} ${load.consignee_address.zip}`;
-            promises.push(queryLocations(query));
-        } else {
-            promises.push(Promise.resolve([]));
-        }
-
-        const [shipperLocations, receiverLocations] = await Promise.all(promises);
-
-        return {
-            shipperLocations,
-            receiverLocations,
-        };
-    };
-
-    const handleAddressConfirmation = (shipperLocationEntry: LocationEntry, receiverLocationEntry: LocationEntry) => {
-        if (shipperLocationEntry) {
-            const { regionText: shipperRegionText } = regionFromLocationEntry(shipperLocationEntry);
-            formHook.setValue('shipper.street', shipperLocationEntry.street);
-            formHook.setValue('shipper.city', shipperLocationEntry.city);
-            formHook.setValue('shipper.state', shipperRegionText);
-            formHook.setValue('shipper.zip', shipperLocationEntry.zip);
-            formHook.setValue('shipper.country', shipperLocationEntry.country.shortCode);
-            formHook.setValue('shipper.longitude', shipperLocationEntry.longitude);
-            formHook.setValue('shipper.latitude', shipperLocationEntry.latitude);
-        }
-
-        if (receiverLocationEntry) {
-            const { regionText: receiverRegionText } = regionFromLocationEntry(receiverLocationEntry);
-            formHook.setValue('receiver.street', receiverLocationEntry.street);
-            formHook.setValue('receiver.city', receiverLocationEntry.city);
-            formHook.setValue('receiver.state', receiverRegionText);
-            formHook.setValue('receiver.zip', receiverLocationEntry.zip);
-            formHook.setValue('receiver.country', receiverLocationEntry.country.shortCode);
-            formHook.setValue('receiver.longitude', receiverLocationEntry.longitude);
-            formHook.setValue('receiver.latitude', receiverLocationEntry.latitude);
-        }
-    };
-
     return (
         <Layout smHeaderComponent={<h1 className="text-xl font-semibold text-gray-900">Create New Load</h1>}>
-            <SaveLoadConfirmation
-                show={showConfirmation}
-                onSave={() => saveLoadData(dataToSave)}
-                onClose={() => setShowConfirmation(false)}
-            />
-            <AddressConfirmation
-                show={showAddressConfirmation}
-                aiLoad={aiLoad}
-                shipperLocations={shipperLocations}
-                receiverLocations={receiverLocations}
-                onConfirm={(shipper: LocationEntry, receiver: LocationEntry) => {
-                    handleAddressConfirmation(shipper, receiver);
-                    setShowAddressConfirmation(false);
-                }}
-                onClose={() => setShowAddressConfirmation(false)}
-            />
             <div className="max-w-4xl py-2 mx-auto">
                 <BreadCrumb
                     className="sm:px-6 md:px-8"
