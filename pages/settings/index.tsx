297df98750250d5bf0228e25ff13323db92051b0
Manny Singh <likebeats@gmail.com>
2023-07-19 17:18:30 -0400

    make fields required

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -18,7 +18,7 @@ type Props = {
 
 const SettingsPage: PageWithAuth<Props> = ({}: Props) => {
     const { data: session } = useSession();
-    const { register, handleSubmit, setValue } = useForm<Carrier>();
+    const { register, handleSubmit, setValue, formState } = useForm<Carrier>();
     const router = useRouter();
 
     const [loadingCarriers, setLoadingCarriers] = useState(true);
@@ -33,15 +33,15 @@ const SettingsPage: PageWithAuth<Props> = ({}: Props) => {
 
     const fields: Array<{ id: keyof Carrier; label: string; required: boolean; type: string }> = [
         { id: 'name', label: 'Company Name', required: true, type: 'input' },
-        { id: 'email', label: 'Contact Email', required: false, type: 'input' },
-        { id: 'phone', label: 'Phone Number', required: false, type: 'input' },
+        { id: 'email', label: 'Contact Email', required: true, type: 'input' },
+        { id: 'phone', label: 'Phone Number', required: true, type: 'input' },
         { id: 'mcNum', label: 'MC Number', required: false, type: 'input' },
         { id: 'dotNum', label: 'DOT Number', required: false, type: 'input' },
-        { id: 'street', label: 'Street Address', required: false, type: 'input' },
-        { id: 'city', label: 'City', required: false, type: 'input' },
-        { id: 'state', label: 'State', required: false, type: 'input' },
-        { id: 'zip', label: 'Zip Code', required: false, type: 'input' },
-        { id: 'country', label: 'Country', required: false, type: 'select' },
+        { id: 'street', label: 'Street Address', required: true, type: 'input' },
+        { id: 'city', label: 'City', required: true, type: 'input' },
+        { id: 'state', label: 'State', required: true, type: 'input' },
+        { id: 'zip', label: 'Zip Code', required: true, type: 'input' },
+        { id: 'country', label: 'Country', required: true, type: 'select' },
     ];
 
     const countryOptions = ['United States', 'Canada', 'Mexico']; // Add or fetch your country list here
@@ -152,7 +152,9 @@ const SettingsPage: PageWithAuth<Props> = ({}: Props) => {
                                                             <input
                                                                 type="text"
                                                                 id={id}
-                                                                {...register(id, { required })}
+                                                                {...register(id, {
+                                                                    required: required ? label + ' is required' : false,
+                                                                })}
                                                                 className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                                             />
                                                         ) : type === 'select' ? (
@@ -170,6 +172,11 @@ const SettingsPage: PageWithAuth<Props> = ({}: Props) => {
                                                                 ))}
                                                             </select>
                                                         ) : null}
+                                                        {formState.errors[id] && (
+                                                            <p className="mt-2 text-sm text-red-600">
+                                                                {formState.errors[id].message}
+                                                            </p>
+                                                        )}
                                                     </div>
                                                 ))}
                                                 <div className="flex px-4 py-4 mt-4 bg-white border-t-2 border-neutral-200">
