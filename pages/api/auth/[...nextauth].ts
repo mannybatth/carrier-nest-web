84eb7d4b926c3358a008c3a4354cf41fc6d5f3ed
Manny Singh <likebeats@gmail.com>
2023-07-24 14:42:10 -0400

    enable allowDangerousEmailAccountLinking

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -37,10 +37,12 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            allowDangerousEmailAccountLinking: true,
         }),
         AzureADProvider({
             clientId: process.env.AZURE_AD_CLIENT_ID,
             clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+            allowDangerousEmailAccountLinking: true,
         }),
     ],
     adapter: PrismaAdapter(prisma),
