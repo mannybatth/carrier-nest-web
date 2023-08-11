6055688328054d4b964831e8693baea33866cc5c
Manny Singh <likebeats@gmail.com>
2023-08-11 01:32:28 -0400

    add conditions to status button

--- pages/loads/[id].tsx
+++ pages/loads/[id].tsx
@@ -27,7 +27,7 @@ import LoadDetailsSkeleton from '../../components/skeletons/LoadDetailsSkeleton'
 import { PageWithAuth } from '../../interfaces/auth';
 import { ExpandedLoad, ExpandedLoadDocument } from '../../interfaces/models';
 import { withServerAuth } from '../../lib/auth/server-auth';
-import { loadStatus, UILoadStatus } from '../../lib/load/load-utils';
+import { isDate24HrInThePast, loadStatus, UILoadStatus } from '../../lib/load/load-utils';
 import {
     addLoadDocumentToLoad,
     deleteLoadById,
@@ -194,6 +194,16 @@ const Toolbar: React.FC<ToolbarProps> = ({
     assignDriverClicked,
     changeLoadStatus,
 }) => {
+    const [dropOffDatePassed, setDropOffDatePassed] = useState(false);
+    const [loadStatusValue, setLoadStatusValue] = useState(null);
+
+    useEffect(() => {
+        if (load) {
+            setDropOffDatePassed(isDate24HrInThePast(new Date(load.receiver.date)));
+            setLoadStatusValue(loadStatus(load));
+        }
+    }, [load]);
+
     return (
         <div className={`flex flex-row place-content-between ${className}`}>
             <span className="inline-flex rounded-md shadow-sm isolate">
@@ -230,81 +240,86 @@ const Toolbar: React.FC<ToolbarProps> = ({
             </span>
 
             <span className="inline-flex rounded-md shadow-sm isolate">
-                {load && (
-                    <div className="inline-flex rounded-md shadow-sm">
-                        <button
-                            type="button"
-                            className="relative inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 bg-white rounded-l-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-                        >
-                            Status: {loadStatus(load).toUpperCase()}
-                        </button>
-                        <Menu as="div" className="relative block -ml-px">
-                            <Menu.Button className="relative inline-flex items-center px-2 py-2 text-gray-400 bg-white rounded-r-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10">
-                                <span className="sr-only">Open options</span>
-                                <ChevronDownIcon className="w-5 h-5" aria-hidden="true" />
-                            </Menu.Button>
-                            <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-100"
-                                enterFrom="transform opacity-0 scale-95"
-                                enterTo="transform opacity-100 scale-100"
-                                leave="transition ease-in duration-75"
-                                leaveFrom="transform opacity-100 scale-100"
-                                leaveTo="transform opacity-0 scale-95"
+                {load &&
+                    !dropOffDatePassed &&
+                    (loadStatusValue === UILoadStatus.BOOKED ||
+                        loadStatusValue === UILoadStatus.IN_PROGRESS ||
+                        loadStatusValue === UILoadStatus.DELIVERED ||
+                        loadStatusValue === UILoadStatus.POD_READY) && (
+                        <div className="inline-flex rounded-md shadow-sm">
+                            <button
+                                type="button"
+                                className="relative inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-900 bg-white rounded-l-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
                             >
-                                <Menu.Items className="absolute right-0 z-10 w-56 mt-2 -mr-1 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                    <div className="py-1">
-                                        {loadStatus(load) !== UILoadStatus.BOOKED && (
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        onClick={() => changeLoadStatus(LoadStatus.CREATED)}
-                                                        className={classNames(
-                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                            'block px-4 py-2 text-sm',
-                                                        )}
-                                                    >
-                                                        Change status to Booked
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        )}
-                                        {loadStatus(load) !== UILoadStatus.IN_PROGRESS && (
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        onClick={() => changeLoadStatus(LoadStatus.IN_PROGRESS)}
-                                                        className={classNames(
-                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                            'block px-4 py-2 text-sm',
-                                                        )}
-                                                    >
-                                                        Change status to In Progress
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        )}
-                                        {loadStatus(load) !== UILoadStatus.DELIVERED && (
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        onClick={() => changeLoadStatus(LoadStatus.DELIVERED)}
-                                                        className={classNames(
-                                                            active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                                            'block px-4 py-2 text-sm',
-                                                        )}
-                                                    >
-                                                        Change status to Delivered
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        )}
-                                    </div>
-                                </Menu.Items>
-                            </Transition>
-                        </Menu>
-                    </div>
-                )}
+                                Status: {loadStatusValue.toUpperCase()}
+                            </button>
+                            <Menu as="div" className="relative block -ml-px">
+                                <Menu.Button className="relative inline-flex items-center px-2 py-2 text-gray-400 bg-white rounded-r-md ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10">
+                                    <span className="sr-only">Open options</span>
+                                    <ChevronDownIcon className="w-5 h-5" aria-hidden="true" />
+                                </Menu.Button>
+                                <Transition
+                                    as={Fragment}
+                                    enter="transition ease-out duration-100"
+                                    enterFrom="transform opacity-0 scale-95"
+                                    enterTo="transform opacity-100 scale-100"
+                                    leave="transition ease-in duration-75"
+                                    leaveFrom="transform opacity-100 scale-100"
+                                    leaveTo="transform opacity-0 scale-95"
+                                >
+                                    <Menu.Items className="absolute right-0 z-10 w-56 mt-2 -mr-1 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                        <div className="py-1">
+                                            {loadStatusValue !== UILoadStatus.BOOKED && (
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <a
+                                                            onClick={() => changeLoadStatus(LoadStatus.CREATED)}
+                                                            className={classNames(
+                                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                'block px-4 py-2 text-sm',
+                                                            )}
+                                                        >
+                                                            Change status to Booked
+                                                        </a>
+                                                    )}
+                                                </Menu.Item>
+                                            )}
+                                            {loadStatusValue !== UILoadStatus.IN_PROGRESS && (
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <a
+                                                            onClick={() => changeLoadStatus(LoadStatus.IN_PROGRESS)}
+                                                            className={classNames(
+                                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                'block px-4 py-2 text-sm',
+                                                            )}
+                                                        >
+                                                            Change status to In Progress
+                                                        </a>
+                                                    )}
+                                                </Menu.Item>
+                                            )}
+                                            {loadStatusValue !== UILoadStatus.DELIVERED && (
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <a
+                                                            onClick={() => changeLoadStatus(LoadStatus.DELIVERED)}
+                                                            className={classNames(
+                                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                'block px-4 py-2 text-sm',
+                                                            )}
+                                                        >
+                                                            Change status to Delivered
+                                                        </a>
+                                                    )}
+                                                </Menu.Item>
+                                            )}
+                                        </div>
+                                    </Menu.Items>
+                                </Transition>
+                            </Menu>
+                        </div>
+                    )}
             </span>
         </div>
     );
