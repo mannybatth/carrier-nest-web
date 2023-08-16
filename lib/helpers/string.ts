ff5029ada3c6e1c06c5deb5ac2491fc0939ffa16
Manny Singh <likebeats@gmail.com>
2023-08-15 23:22:35 -0400

    fix bug

--- lib/helpers/string.ts
+++ lib/helpers/string.ts
@@ -3,6 +3,6 @@ export const sanitize = (str: string) => {
         .toString() // Convert to string
         .toLowerCase() // Convert to lowercase
         .replace(/\s+/g, '_') // Replace spaces with underscores
-        .replace(/[^\w.-]+/g, '') // Remove all non-word characters except for dot and dash
-        .replace(/\.+/g, '.'); // Replace multiple dots with a single dot
+        .replace(/[^\w-]+/g, '') // Remove all non-word characters except for dash
+        .replace(/\.+/g, ''); // Remove all dots
 };
