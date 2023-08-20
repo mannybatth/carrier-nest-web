61023516dd50433a07beaf0111962aa7a2105185
Manny Singh <likebeats@gmail.com>
2023-08-20 15:55:33 -0400

    drop routeId field

---
+++ prisma/migrations/20230820195450_remove_route_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `routeId` on the `Load` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Load" DROP COLUMN "routeId";
