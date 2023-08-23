34e5eb45e029b68b0fa276e436913d29d0459970
Manny Singh <likebeats@gmail.com>
2023-08-23 00:30:02 -0400

    add logo

--- components/layout/SideBarFooter.tsx
+++ components/layout/SideBarFooter.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image';
 import React from 'react';
+import CarrierNextImage from '../images/CarrierNextImage';
+import LogoSlogan from '../images/LogoSlogan';
 
 const SideBarFooter: React.FC = () => (
-    <div className="flex items-center justify-center flex-shrink-0 mb-5">
-        <span className="text-xl font-bold text-gray-800">Carrier Nest</span>
+    <div className="flex flex-col items-center justify-center flex-shrink-0 mb-8">
+        <Image
+            src="/logo_truck_black.svg"
+            alt="Logo"
+            width={100}
+            height={93}
+            className="w-[100px] opacity-30 mb-4"
+        ></Image>
+        <CarrierNextImage className="mb-2" />
+        <LogoSlogan />
     </div>
 );
 
