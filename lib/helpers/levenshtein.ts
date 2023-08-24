6a5594daddd75431cbbd73c56068e2b128350c62
Manny Singh <likebeats@gmail.com>
2023-08-23 21:11:33 -0400

    checkpoint

--- lib/helpers/levenshtein.ts
+++ lib/helpers/levenshtein.ts
@@ -1,33 +1,85 @@
-import { Searcher } from 'fast-fuzzy';
+// import { Searcher } from 'fast-fuzzy';
 
-export function fuzzySearch(query: string, list: string[]): number {
-    // Clean the query
-    const cleanedQuery = cleanQuery(query);
-
-    // Clean the list
-    const cleanedList = list.map((item) => cleanQuery(item));
-
-    // Do the search
-    const searcher = new Searcher(cleanedList);
-    const bestMatch = searcher.search(cleanedQuery)[0];
-
-    // Return the index of the best match in the original list
-    return cleanedList.indexOf(bestMatch);
+function cleanString(str) {
+    // Remove non-alphanumeric characters and extra spaces
+    return str
+        .replace(/[^a-zA-Z0-9\s]+/g, '')
+        .trim()
+        .toLowerCase();
 }
 
-function cleanQuery(query: string): string {
-    // Define the unwanted words
-    const unwantedWords = ['logistics', 'transport', 'llc', ',', '.'];
+export function fuzzySearch(query, customers) {
+    const normalizedQuery = cleanString(query);
 
-    // Remove unwanted characters
-    query = query.replace(/[,.]/g, '');
+    // First, try partial matching
+    for (let i = 0; i < customers.length; i++) {
+        const normalizedCustomer = cleanString(customers[i]);
+        if (normalizedCustomer.includes(normalizedQuery)) {
+            return i;
+        }
+    }
 
-    // Split the query into words
-    let words = query.split(' ');
+    // Weighted token-based matching
+    let bestMatchIndex = -1;
+    let minWeightedDistance = Infinity;
 
-    // Remove unwanted words
-    words = words.filter((word) => !unwantedWords.includes(word.toLowerCase()));
+    const queryTokens = normalizedQuery.split(' ');
 
-    // Join the words back together into a string
-    return words.join(' ');
+    for (let i = 0; i < customers.length; i++) {
+        const normalizedCustomer = cleanString(customers[i]);
+
+        let weightedDistance = 0;
+        for (const token of queryTokens) {
+            const distance = levenshtein(token, normalizedCustomer);
+            weightedDistance += distance / (token.length + 1);
+        }
+
+        if (weightedDistance < minWeightedDistance) {
+            minWeightedDistance = weightedDistance;
+            bestMatchIndex = i;
+        }
+    }
+
+    // Use a dynamic threshold based on the number of tokens in the query
+    const threshold = queryTokens.length * 0.6; // Adjusted threshold
+    if (minWeightedDistance > threshold) {
+        return -1;
+    }
+
+    return bestMatchIndex;
+}
+
+function levenshtein(str1, str2) {
+    // If either string is empty, return the length of the other string
+    if (str1.length === 0) return str2.length;
+    if (str2.length === 0) return str1.length;
+
+    const matrix = [];
+
+    // Increment along the first column of each row
+    for (let i = 0; i <= str2.length; i++) {
+        matrix[i] = [i];
+    }
+
+    // Increment each column in the first row
+    for (let j = 0; j <= str1.length; j++) {
+        matrix[0][j] = j;
+    }
+
+    // Fill in the rest of the matrix
+    for (let i = 1; i <= str2.length; i++) {
+        for (let j = 1; j <= str1.length; j++) {
+            if (str2.charAt(i - 1) === str1.charAt(j - 1)) {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            } else {
+                matrix[i][j] = Math.min(
+                    matrix[i - 1][j - 1] + 1, // Substitution
+                    matrix[i][j - 1] + 1, // Insertion
+                    matrix[i - 1][j] + 1, // Deletion
+                );
+            }
+        }
+    }
+
+    return matrix[str2.length][str1.length];
 }
