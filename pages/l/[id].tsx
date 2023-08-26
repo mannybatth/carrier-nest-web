9f6832aafbcfc875a10cc15fc743a575283cae3c
Manny Singh <likebeats@gmail.com>
2023-08-26 14:22:57 -0400

    add notify on error

--- pages/l/[id].tsx
+++ pages/l/[id].tsx
@@ -128,6 +128,7 @@ const DriverLoadDetailsPage: PageWithAuth = () => {
                     },
                     (error) => {
                         setFetchingLocation(false);
+                        notify({ title: 'Unable to get location', message: error.message, type: 'error' });
                         resolve({
                             longitude: null,
                             latitude: null,
