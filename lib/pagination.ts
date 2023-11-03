73bcde6ad6d4d5af8c4aaaab06e5ce9371692640
Manny Singh <likebeats@gmail.com>
2023-11-03 15:18:43 -0400

    remove logs

--- lib/pagination.ts
+++ lib/pagination.ts
@@ -10,9 +10,6 @@ export const calcPaginationMetadata = ({ total, limit, offset }: { total: number
     const prevOffset = Math.max(0, offset - limit);
     const nextOffset = Math.min(total, offset + limit);
 
-    console.log('prevOffset', prevOffset);
-    console.log('nextOffset', nextOffset);
-
     if (prevOffset >= 0 && offset > 0) {
         metadata.prev = {
             offset: prevOffset,
