c31c734e47c345da4dee40d2256c4b0bdde3bc7e
Manny Singh <likebeats@gmail.com>
2024-07-12 21:43:36 -0400

    cleanup

--- constants.ts
+++ constants.ts
@@ -3,7 +3,3 @@ export const appUrl =
         ? 'http://localhost:3000' // development api
         : process.env.NEXT_PUBLIC_VERCEL_URL;
 export const apiUrl = `${appUrl}/api`;
-export const pythonApiUrl =
-    process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3001' // development api
-        : process.env.PYTHON_API_URL; // production api
