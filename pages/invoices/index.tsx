f95e055b8bf361041802e017b9332e38f780ce48
Manny Singh <likebeats@gmail.com>
2024-12-29 16:45:17 -0500

    more renames

--- pages/invoices/index.tsx
+++ pages/invoices/index.tsx
@@ -18,7 +18,7 @@ import { queryFromPagination, queryFromSort, sortFromQuery } from '../../lib/hel
 import { deleteInvoiceById, getAccountingStats, getInvoicesExpanded } from '../../lib/rest/invoice';
 import { useLocalStorage } from '../../lib/useLocalStorage';
 
-const AccountingPage: PageWithAuth = () => {
+const InvoicesPage: PageWithAuth = () => {
     const searchParams = useSearchParams();
     const sortProps = sortFromQuery({
         sortBy: searchParams.get('sortBy'),
@@ -152,7 +152,7 @@ const AccountingPage: PageWithAuth = () => {
         <Layout
             smHeaderComponent={
                 <div className="flex items-center">
-                    <h1 className="flex-1 text-xl font-semibold text-gray-900">Accounting</h1>
+                    <h1 className="flex-1 text-xl font-semibold text-gray-900">Invoices</h1>
                 </div>
             }
         >
@@ -179,7 +179,7 @@ const AccountingPage: PageWithAuth = () => {
                 <div className="py-2 mx-auto max-w-7xl">
                     <div className="hidden px-5 my-4 md:block sm:px-6 md:px-8">
                         <div className="flex">
-                            <h1 className="flex-1 text-2xl font-semibold text-gray-900">Accounting</h1>
+                            <h1 className="flex-1 text-2xl font-semibold text-gray-900">Invoices</h1>
                         </div>
                         <div className="w-full mt-2 mb-1 border-t border-gray-300" />
                     </div>
@@ -356,6 +356,6 @@ const AccountingPage: PageWithAuth = () => {
     );
 };
 
-AccountingPage.authenticationEnabled = true;
+InvoicesPage.authenticationEnabled = true;
 
-export default AccountingPage;
+export default InvoicesPage;
