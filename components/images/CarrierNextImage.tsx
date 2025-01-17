5bfe74c96e248e9a0f067c7416233203e91e6f24
Sandeep <sandeeppenaich@outlook.com>
2025-01-16 21:48:43 -0800

    adjust sidebar layout and footer placement

--- components/images/CarrierNextImage.tsx
+++ components/images/CarrierNextImage.tsx
@@ -7,13 +7,13 @@ type Props = {
 const CarrierNextImage: React.FC<Props> = ({ className }: Props) => {
     return (
         <div className={className}>
-            <svg width="140" height="20" viewBox="0 0 425 46" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <svg width="120" height="20" viewBox="0 0 425 30" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <title>CARRIER NEST</title>
                 <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
                     <g
                         id="Large-Copy"
                         transform="translate(-871.000000, -196.000000)"
-                        fill="#929292"
+                        fill="#29465B"
                         fillRule="nonzero"
                     >
                         <g id="CARRIER-NEST" transform="translate(871.730953, 196.648000)">
--- components/layout/SideBarFooter.tsx
+++ components/layout/SideBarFooter.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import CarrierNextImage from '../images/CarrierNextImage';
 import LogoSlogan from '../images/LogoSlogan';
+import Image from 'next/image';
 
 const SideBarFooter: React.FC = () => (
-    <div className="flex flex-col items-center justify-center flex-shrink-0 mb-8">
-        <CarrierNextImage className="mb-2" />
-        <LogoSlogan />
+    <div className="flex flex-col items-center justify-center flex-shrink-0 mb-0  ">
+        <div className="relative flex flex-row gap-2 mt-1 items-end justify-start w-full h-full p-4 text-center rounded-md">
+            <div className="flex">
+                <Image src={'/logo_truck_100.png'} height={60} width={60} alt="Carrier Nest Logo" />
+            </div>
+            <div className="flex flex-col items-start">
+                <CarrierNextImage className="mb-0" />
+                <LogoSlogan />
+            </div>
+        </div>
     </div>
 );
 
