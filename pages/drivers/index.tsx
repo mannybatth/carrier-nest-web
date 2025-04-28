322c63e117feae3104792cd6056cbcb6f276fd68
Sandeep <sandeeppenaich@outlook.com>
2025-04-27 22:39:04 -0700

    style: enhance layout and styling of Drivers header section

--- pages/drivers/index.tsx
+++ pages/drivers/index.tsx
@@ -193,19 +193,18 @@ const DriversPage: PageWithAuth = () => {
                             </div>
                         </div>
                     )}
-                    <div className="hidden px-5 my-4 md:block sm:px-6 md:px-8">
-                        <div className="flex">
-                            <h1 className="flex-1 text-2xl font-semibold text-gray-900">Drivers</h1>
+                    <div className="hidden px-5 my-4 md:block sm:px-2 md:px-4 mx-8 py-4 pb-5 -mb-2 mt-0 bg-white shadow-md  border-gray-200  border-t border-l border-r rounded-tl-lg rounded-tr-lg">
+                        <div className="flex flex-col md:flex-row items-center justify-between">
+                            <h1 className=" flex-1 text-xl font-bold text-gray-800">Drivers</h1>
                             <Link href="/drivers/create">
                                 <button
                                     type="button"
-                                    className="inline-flex items-center px-3.5 py-2 border border-transparent text-sm leading-4 font-medium rounded-full shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                                    className="inline-flex items-center px-3.5 h-full py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                                 >
                                     + Create Driver
                                 </button>
                             </Link>
                         </div>
-                        <div className="w-full mt-2 mb-1 border-t border-gray-300" />
                     </div>
                     <div className="px-5 sm:px-6 md:px-8">
                         {loadingDrivers ? (
