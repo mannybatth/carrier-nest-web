ee293a7be9acfc0706e94e50baadf7c2d52574d7
Sandeep <sandeeppenaich@outlook.com>
2025-05-07 21:57:16 -0700

    style: enhance empty state styling in LoadsTableCompact for better visibility

--- components/loads/LoadsTableCompact.tsx
+++ components/loads/LoadsTableCompact.tsx
@@ -245,7 +245,7 @@ export const LoadsTableCompact: React.FC<Props> = ({
             sort={sort}
             changeSort={changeSort}
             emptyState={
-                <div className="my-5 text-center">
+                <div className="my-0 text-center border border-gray-200 rounded-lg shadow-sm p-6 bg-white">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="w-12 h-12 mx-auto text-gray-400"
